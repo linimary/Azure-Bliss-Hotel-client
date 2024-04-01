568311e7d36f9d7f1d7bbf658dcd066537920064
linimary <mariya.lichanova@gmail.com>
2024-04-01 20:43:52 +0300

    Added Footer, NavBar, Room and RoomCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import './App.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import AddRoom from './components/room/AddRoom.jsx'
-import ExistingRooms from './components/room/ExistingRooms.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/home/Home.jsx'
-import EditRoom from './components/room/EditRoom.jsx'
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import AddRoom from "./components/room/AddRoom.jsx";
+import ExistingRooms from "./components/room/ExistingRooms.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import EditRoom from "./components/room/EditRoom.jsx";
+import NavBar from "./components/layout/NavBar.jsx";
+import Footer from "./components/layout/Footer.jsx";
 
 function App() {
-
   return (
     <>
-    <main>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/edit-room/:roomId' element={<EditRoom />} />
-          <Route path='/existing-rooms' element={<ExistingRooms />} />
-          <Route path='/add-room' element={<AddRoom />} />
-        </Routes>
-      </Router>
-    </main>
+      <main>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="/add-room" element={<AddRoom />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/room/RoomListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomListing = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RoomListing
